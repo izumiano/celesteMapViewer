2ef49ba621e1ed525359a0e93fbff70299fa1f80
izumiano <elibrine@gmail.com>
2025-05-11 13:34:31 +0200

    show contents of lua map table

--- lua/test.lua
+++ lua/test.lua
@@ -1,4 +1,17 @@
 local mapcoder = require("mapcoder")
 
+local function dump(o)
+	if type(o) == 'table' then
+		local s = '{ '
+		for k,v in pairs(o) do
+			if type(k) ~= 'number' then k = '"'..k..'"' end
+			s = s .. '['..k..'] = ' .. dump(v) .. ',\n'
+		end
+		return s .. '} '
+	else
+		return tostring(o)
+	end
+end
+
 local path = "C:\\Users\\elibr\\Dev\\Web\\celesteMapViewer\\testData\\6-ReflectionD-B.bin";
-mapcoder.decodeFile(path)
+print(dump(mapcoder.decodeFile(path)))
