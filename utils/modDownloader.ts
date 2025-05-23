f16dc342373dc547d0998206b09058d663fb2c7a
izumiano <izumiano@users.noreply.github.com>
2025-05-23 09:34:05 +0200

    more explicit code style

--- utils/modDownloader.ts
+++ utils/modDownloader.ts
@@ -1,6 +1,6 @@
-export default class ModDownloader {
-  static corsProxy = 'https://cors-header-proxy.izumiano.workers.dev';
+const corsProxy = 'https://cors-header-proxy.izumiano.workers.dev';
 
+export default class ModDownloader {
   async download(url: string) {
     const modDownloadLink = await this.#getModDownloadLink(url);
 
@@ -8,13 +8,13 @@ export default class ModDownloader {
       throw new Error('Download link is an empty string');
     }
 
-    const request = `${ModDownloader.corsProxy}/?url=${encodeURIComponent(modDownloadLink)}`;
+    const request = `${corsProxy}/?url=${encodeURIComponent(modDownloadLink)}`;
     console.log(request);
 
     return await fetch(request);
   }
 
-  async #getModDownloadLink(modLink: string) {
+  async #getModDownloadLink(modLink: string): Promise<string> {
     const gbModLink = modLink.match(/gamebanana.com\/mods\/(\d+)/);
     if (gbModLink) {
       try {
