02f44faf0b499ca65ef185f1b5d8a26f104b9800
izumiano <izumiano@users.noreply.github.com>
2025-06-23 23:27:48 +0200

    properly render spinners

--- map/mapTypes/entities/spinner.ts
+++ map/mapTypes/entities/spinner.ts
@@ -1,10 +1,66 @@
+import {getRandomInt} from '../../../utils/math.js';
 import Result from '../../../utils/result.js';
-import {CelesteMap} from '../celesteMap.js';
+import {Vector2} from '../../utils/vector2.js';
 import {Entity} from './entity.js';
 
+type SpinnerColor = 'blue' | 'core' | 'purple' | 'rainbow' | 'red';
+type SpinnerTextures = 'blue' | 'red' | 'purple' | 'white';
+
+const imageDir = window.location + 'assets/spinners/';
+const textureMappings: Map<SpinnerColor, SpinnerTextures> = new Map([
+  ['blue', 'blue'],
+  ['core', 'red'],
+  ['purple', 'purple'],
+  ['rainbow', 'white'],
+  ['red', 'red'],
+]);
+
 export default class Spinner extends Entity {
+  static images: Map<string, HTMLImageElement> = new Map();
+
+  color: SpinnerColor;
+  spinnerTextureIndex: number;
+
   constructor(entity: any) {
     super(entity, -8500);
+    this.color = entity.color?.toLowerCase() ?? 'blue';
+    this.spinnerTextureIndex = getRandomInt(0, 3, entity.id);
+  }
+
+  static async #getImage(path: string) {
+    if (this.images.has(path)) {
+      return this.images.get(path)!;
+    }
+
+    const image = new Image();
+    await new Promise((resolve, reject) => {
+      image.onload = () => {
+        resolve(null);
+      };
+      image.onerror = () => {
+        reject('Failed loading image');
+      };
+      image.src = imageDir + path;
+    });
+    this.images.set(path, image);
+    return image;
+  }
+
+  static async getImage(color: SpinnerColor, textureIndex: number) {
+    const path = `crystal/fg_${textureMappings.get(color)}0${textureIndex}}.png`;
+    if (this.images.has(path)) {
+      return this.images.get(path)!;
+    }
+
+    let image = new Image();
+    try {
+      image = await this.#getImage(path);
+    } catch (ex) {
+      console.warn(ex);
+      image = await this.#getImage('crystal/fg_blue00.png');
+    }
+    this.images.set(path, image);
+    return image;
   }
 
   async draw(
@@ -14,17 +70,16 @@ export default class Spinner extends Entity {
     scale: number,
     abortController: AbortController,
   ) {
-    ctx.beginPath();
-    ctx.arc(
-      this.x * scale + xOffset,
-      this.y * scale + yOffset,
-      CelesteMap.tileMultiplier * scale,
-      0,
-      Math.PI * 2,
-      true,
+    const image = await Spinner.getImage(this.color, this.spinnerTextureIndex);
+    const imageScale = new Vector2(image.width * scale, image.height * scale);
+
+    ctx.drawImage(
+      image,
+      this.x * scale + xOffset - imageScale.x / 2,
+      this.y * scale + yOffset - imageScale.y / 2,
+      imageScale.x,
+      imageScale.y,
     );
-    ctx.closePath();
-    ctx.stroke();
 
     return Result.success();
   }
--- utils/math.ts
+++ utils/math.ts
@@ -22,6 +22,36 @@ export function plateauingSineEase(val: number, l: number, r: number) {
   }
 }
 
-export function getRandomInt(min: number, max: number) {
+export function getRandomInt(
+  min: number,
+  max: number,
+  seed: number | null = null,
+) {
+  if (seed) {
+    return new SeededRNG(seed).nextInt(min, max);
+  }
   return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+class SeededRNG {
+  state: number;
+
+  constructor(seed: number) {
+    this.state = Math.abs(Math.floor(seed));
+  }
+
+  next() {
+    const a = 1103515245;
+    const c = 12345;
+    const m = Math.pow(2, 31);
+
+    this.state = (a * this.state + c) % m;
+    return this.state / m;
+  }
+
+  nextInt(min: number, max: number) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(this.next() * (max - min + 1)) + min;
+  }
 }
